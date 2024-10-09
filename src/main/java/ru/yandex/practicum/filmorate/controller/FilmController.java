2e60aa6a999792cfc18b5dbbfff7f9e489116672
Nikolay Lugovoy <enelve@yandex.ru>
2024-10-09 23:59:13 +0300

    Вывод самых популярных фильмов по жанру и годамD

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -9,6 +9,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -50,8 +51,11 @@ public class FilmController {
     }
 
     @GetMapping("/popular")
-    public Collection<FilmDto> getMostPopular(@RequestParam(defaultValue = "10") Integer count) {
-        return filmService.getMostPopular(count).stream().map(FilmMapper::toDto).toList();
+    public Collection<FilmDto> getMostPopular(
+            @RequestParam(value = "count", defaultValue = "10") Integer count,
+            @RequestParam(value = "genreId", required = false) Optional<Integer> genreId,
+            @RequestParam(value = "year", required = false) Optional<Integer> year) {
+        return filmService.getMostPopularByGenreAndYear(count, genreId, year).stream().map(FilmMapper::toDto).toList();
     }
 
     @DeleteMapping("{id}")
